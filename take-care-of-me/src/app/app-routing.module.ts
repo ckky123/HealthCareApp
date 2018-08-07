b0fb49a3092330f6074a7cdb7624d9231c4d3d73
Wendy Lee <wendy.lee@aveva.com>
2018-08-07 14:57:00 +0100

    add new icon

--- take-care-of-me/src/app/app-routing.module.ts
+++ take-care-of-me/src/app/app-routing.module.ts
@@ -1,31 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { FindCarePeopleComponent } from './find-care-people/find-care-people.component';
 import { BeCarePeopleComponent } from './be-care-people/be-care-people.component';
 
 const routes: Routes = [
   {
     path: 'find',
-    component: HomeComponent,
+    component: FindCarePeopleComponent,
     resolve: {
     },
-    canActivate: [],
     children: []
   },
   {
-    path: 'become',
-    component: HomeComponent,
+    path: 'be',
+    component: BeCarePeopleComponent,
     resolve: {
     },
-    canActivate: [],
     children: []
   }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
