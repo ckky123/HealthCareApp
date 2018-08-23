3cca5c4bacaec25c50d167b3d71fd8fc106fd706
Wendy Lee <wendy.lee@aveva.com>
2018-08-23 12:22:41 +0100

    fix version issue

--- take-care-of-me/src/app/be-care-people/be-care-people.component.ts
+++ take-care-of-me/src/app/be-care-people/be-care-people.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-be-care-people',
   templateUrl: './be-care-people.component.html',
-  styleUrls: ['./be-care-people.component.css']
+  styleUrls: ['./be-care-people.component.scss']
 })
 export class BeCarePeopleComponent implements OnInit {
 
--- take-care-of-me/src/app/find-care-people/find-care-people.component.ts
+++ take-care-of-me/src/app/find-care-people/find-care-people.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-find-care-people',
   templateUrl: './find-care-people.component.html',
-  styleUrls: ['./find-care-people.component.css']
+  styleUrls: ['./find-care-people.component.scss']
 })
 export class FindCarePeopleComponent implements OnInit {
 
